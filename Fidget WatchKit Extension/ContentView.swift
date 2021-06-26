d3945c802439b8bcfaf5849906b98bae7f10761d
Mark Schmidt <mark.schmidt@hey.com>
2021-06-26 14:23:16 -0500

    Initial Commit

---
+++ Fidget WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fidget WatchKit Extension
+//
+//  Created by Mark Schmidt on 6/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fidget WatchKit Extension/FidgetApp.swift
@@ -0,0 +1,19 @@
+//
+//  FidgetApp.swift
+//  Fidget WatchKit Extension
+//
+//  Created by Mark Schmidt on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct FidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
