c90add8c6d49c572dc9f1fedb347c2d7259b4e4d
Mark Schmidt <mark.schmidt@hey.com>
2022-09-17 22:40:00 -0500

    Update version

--- Fidget WatchKit Extension/FidgetApp.swift
+++ Fidget WatchKit Extension/FidgetApp.swift
@@ -16,21 +16,32 @@ func watchHaptic(velocity: Double) {
 func getFrame(_ deviceBounds: CGSize) -> Frame{
     switch deviceBounds.width {
     case 136: // 38mm
+        print("Detected 38 mm")
         return Frame(bottom: deviceBounds.height - 19.0, right: deviceBounds.width, cornerRadius: 0.0)
     case 156: // 42mm
+        print("Detected 42 mm")
         return Frame(bottom: deviceBounds.height - 21.0, right: deviceBounds.width, cornerRadius: 0.0)
     case 162: // 40mm
+        print("Detected 40 mm")
         let offset = 28.0
         return Frame(top: -offset, bottom: deviceBounds.height - offset + 1.0, left: -1.0, right: deviceBounds.width , cornerRadius: 30.0)
     case 184: // 44mm
+        print("Detected 44 mm")
         let offset = 31.0
         return Frame(top: -offset, bottom: deviceBounds.height - offset, left: -1.0, right: deviceBounds.width, cornerRadius: 35.0)
     case 176.0: // 41 mm
+        print("Detected 41 mm")
         let offset = 34.0
         return Frame(top: -offset, bottom: deviceBounds.height - offset, left: -1.0, right: deviceBounds.width - 1.0, cornerRadius: 40.0)
     case 198.0: // 45 mm
+        print("Detected 45 mm")
         let offset = 35.0
         return Frame(top: -offset, bottom: deviceBounds.height - offset, left: -1.0, right: deviceBounds.width - 1.0, cornerRadius: 42.0)
+    case 205.0:
+        print("Detected 49 mm")
+        let offset = 37.0
+        return Frame(top: -offset, bottom: deviceBounds.height - offset, left: -3.5, right: deviceBounds.width - 5.0, cornerRadius: 54.0)
+        
     default:
         print("Unknown watch with width: \(deviceBounds.width)")
         return Frame(bottom: deviceBounds.height, right: deviceBounds.width, cornerRadius: 35.0)
