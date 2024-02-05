73d9f2768a08eff1b1d0cba032dc317388e1a455
Mark Schmidt <mark.schmidt@hey.com>
2024-02-05 13:57:08 -0600

    Prettify Reorder View

--- Fidget WatchKit Extension/ReorderView.swift
+++ Fidget WatchKit Extension/ReorderView.swift
@@ -26,6 +26,7 @@ enum FidgetViewType : Int, Identifiable {
         case .breathe: return "Square Breathing"
         }
     }
+    
 }
 
 let defaultOrder : [FidgetViewType] = [.crown, .accelerometer, .joystick, .lightswitch, .button, .breathe]
@@ -38,16 +39,48 @@ func stringToOrder(_ string: String) -> [FidgetViewType] {
     string.split(separator: ",").map({ rawValue in FidgetViewType(rawValue: Int(rawValue)!)!})
 }
 
+
 struct ReorderView : View {
    
     @AppStorage("order") var orderString = orderToString(defaultOrder)
+    let shapeSize = 20.0
     
     var body: some View {
         NavigationView {
             List{
                 Section {
                     ForEach(stringToOrder(orderString)) { viewType in
-                        Text("\(viewType.getName())")
+                        HStack {
+                            Text("\(viewType.getName())")
+                            Spacer()
+                            switch viewType {
+                            case .crown:
+                                Triangle().stroke(.purple).frame(width: shapeSize, height: shapeSize)
+                            case .accelerometer:
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 5.0).stroke(.purple)
+                                    Circle().stroke(.purple).frame(width: 5.0, height: 5.0)
+                                }.frame(width: shapeSize, height: shapeSize)
+                            case .joystick:
+                                ZStack {
+                                    Circle().fill(.purple).opacity(0.5)
+                                    Circle().stroke(.purple).frame(width: 10.0, height: 10.0)
+                                }.frame(width: shapeSize, height: shapeSize)
+                            case .lightswitch:
+                                VStack(spacing: 0.0) {
+                                    MSRoundRectangle(round: [.top], radius: 5.0).stroke(.purple)
+                                    ZStack {
+                                        MSRoundRectangle(round: [.bottom], radius: 5.0).fill(.purple)
+                                        MSRoundRectangle(round: [.bottom], radius: 5.0).stroke(.purple)
+                                    }
+                                }.frame(width: shapeSize, height: shapeSize)
+                            case .button:
+                                RoundedRectangle(cornerRadius: 3.0).fill(.purple).frame(width: shapeSize, height: 10.0)
+                            case .breathe:
+                                Rectangle().stroke(.purple).frame(width: shapeSize, height: shapeSize)
+                            }
+                            
+                        }
                     }
                     .onMove { from, to in
                         var order = stringToOrder(orderString)
@@ -56,7 +89,7 @@ struct ReorderView : View {
                         orderString = orderToString(order)
                     }
                 } header: {
-                    Text("Reorder Widgets")
+                    Text("Reorder Fidgets")
                 }
             }
         }
